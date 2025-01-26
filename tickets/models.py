db8e05d7a25f1e01aaf54dd571252feb5e0692e3
bassant hossam <125453475+Bassanthossamxx@users.noreply.github.com>
2025-01-27 01:25:09 +0200

    start and added models

---
+++ tickets/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+#how to write models :
+#what need in tickets :
+# guest  -- Movie -- Reservation
+# models :
+#1- for Movie
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    hall = models.CharField(max_length=10)
+    date = models.DateField()
+
+class Guest(models.Model):
+    name = models.CharField(max_length=30)
+    Phone = models.CharField(max_length=15)
+
+class Reservation(models.Model):
+    guest = models.ForeignKey(Guest, related_name='reservation',on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, related_name='reservation' ,on_delete=models.CASCADE)
+    time = models.TimeField()
+    price = models.IntegerField()
+    payment_method = models.CharField(max_length=20)
