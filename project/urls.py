aea4848e4ef715be6d0be8f3a5a746b74bd13722
bassant hossam <125453475+Bassanthossamxx@users.noreply.github.com>
2025-01-29 18:25:19 +0200

    add comments to urls

--- project/urls.py
+++ project/urls.py
@@ -1,13 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path
-
 from tickets import views
 
-urlpatterns = [
+urlpatterns = \
+    [
     path('admin/', admin.site.urls),
     #model And No Rest Framework
     path('UsingModelNoRest/',views.FBV_Model_No_Rest),
-    path('rest/guests/',views.FBV_Get_Post_Guest),
-    path('rest/guest/<int:pk>/' , views.FBV_Get_Put_Delete_Guest)
-]
+    #FBVs & Rest Framework
+    path('rest/guests/',views.FBV_Get_Post_Guest), #Get and Post
+    path('rest/guest/<int:pk>/' , views.FBV_Get_Put_Delete_Guest) #delete , update , get using ID
+    ]
