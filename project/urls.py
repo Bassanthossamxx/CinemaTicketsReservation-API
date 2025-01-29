4688d6b97d7a94c3d26199b10673250c7b2d64e5
bassant hossam <125453475+Bassanthossamxx@users.noreply.github.com>
2025-01-29 18:23:23 +0200

    Added two Function based views rest framework to cover all methods and added urls of them

--- project/urls.py
+++ project/urls.py
@@ -7,5 +7,7 @@ from tickets import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     #model And No Rest Framework
-    path('ModelNoRest/',views.FBVModelNoRest),
+    path('UsingModelNoRest/',views.FBV_Model_No_Rest),
+    path('rest/guests/',views.FBV_Get_Post_Guest),
+    path('rest/guest/<int:pk>/' , views.FBV_Get_Put_Delete_Guest)
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -1,22 +1,54 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from .models import *
+from .serializers import GuestSerializer
 
 
-#Function based view model and no rest framework:
-def FBVModelNoRest(request):
-    dataGuest = Guest.objects.all()
-    #dataReservation = Reservation.objects.all()
-    #dataMovie = Movie.objects.all()
-    response = \
-        {
-        'guests': list(dataGuest.values()) ,
-            #if I need to change it "practise"
-        # 'movies' : list(dataMovie.values()),
-        # 'reservations' : list(dataReservation.values())  ,
-        }
+def FBV_Model_No_Rest(request):
+    guest = Guest.objects.all()
+    response = {
+        'guests': list(guest.values()),
+    }
     return JsonResponse(response)
 
 
+@api_view(['GET', 'POST'])
+def FBV_Get_Post_Guest(request):
+    if request.method == 'GET':
+        guests = Guest.objects.all()
+        serializer = GuestSerializer(guests, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = GuestSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(['GET', 'PUT', 'DELETE'])
+def FBV_Get_Put_Delete_Guest(request, pk):
+    try:
+        guest = Guest.objects.get(pk=pk)
+    except Guest.DoesNotExist:
+        return Response({"error": "Guest not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = GuestSerializer(guest)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'PUT':
+        serializer = GuestSerializer(guest, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        guest.delete()
+        guests = Guest.objects.all()
+        serializer = GuestSerializer(guests, many=True)
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
